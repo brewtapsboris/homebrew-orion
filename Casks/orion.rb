8bda847a56c97426b0146e2e280a58d2f3015c6a
brewtapsboris <brewtapsboris@gmail.com>
2025-12-27 22:25:42 -0800

    since Orion is a Cask (a macOS app), the syntax for headers is slightly different

--- Casks/orion.rb
+++ Casks/orion.rb
@@ -2,11 +2,8 @@ cask "orion" do
   version "1.0"
   sha256 :no_check
 
-  # We add a User-Agent header here to prevent Kagi's server from blocking the download
   url "https://browser.kagi.com/updates/macos/Orion.dmg",
-      headers: {
-        "User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
-      }
+      user_agent: :fake
       
   name "Orion Browser"
   desc "WebKit based, ad-free browser"
